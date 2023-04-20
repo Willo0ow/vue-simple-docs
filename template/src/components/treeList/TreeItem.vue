cb16d42f3a2de58459f0b953faf06b6ffdccfd04
Willo0ow <dhinc39@gmail.com>
2023-04-20 10:08:36 +0200

    display nested source directory structure

---
+++ template/src/components/treeList/TreeItem.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-list-item>
+    <v-list-item-title
+      ><router-link
+        :to="{
+          name: 'ComponentDetails',
+          params: { name: item.name },
+          query: { prefix: item.prefix }
+        }"
+        >{{ item.name }}</router-link
+      >
+    </v-list-item-title>
+    <v-list-item-subtitle v-if="item?.children?.length">
+      <v-list lines="two">
+        <tree-item
+          v-for="child in item.children"
+          :item="child"
+          :key="`${child.prefix}${child.name}`"
+        />
+      </v-list>
+    </v-list-item-subtitle>
+  </v-list-item>
+</template>
+
+<script setup lang="ts">
+import { type FileSystemObject } from '@/types/generated'
+/**
+ * @vueProp {number} [param=0] - number of params
+ */
+
+defineProps<{
+  item: FileSystemObject
+}>()
+</script>
