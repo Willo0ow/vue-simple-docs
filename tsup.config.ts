773686974fff895a0f0e5fef1687aa82363f9932
Willo0ow <dhinc39@gmail.com>
2023-04-21 10:26:55 +0200

    clean ts configs

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,8 +1,13 @@
-import { defineConfig } from 'tsup'
+import { defineConfig } from 'tsup';
 
 export default defineConfig({
   entry: ['lib/index.ts', 'lib/generateData.ts', 'lib/deleteGeneratedData.ts'],
   splitting: false,
   sourcemap: false,
   clean: true,
-})
+  format: ['cjs', 'esm'],
+  dts: false,
+  minify: true,
+  target: 'node14',
+  outDir: 'dist'
+});
