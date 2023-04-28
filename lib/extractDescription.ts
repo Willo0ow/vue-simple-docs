0548f187097e3defba7e75d7faeaaadf5b71780e
Willo0ow <dhinc39@gmail.com>
2023-04-28 09:39:29 +0200

    delete speciofic tag extraction functions

--- lib/extractDescription.ts
+++
@@ -1,5 +0,0 @@
-export default (documentationTagsSection: string) => {
-  const descriptionRegex = /@vueDescription\s+([\w-]+)/;
-  const descriptionMatch = descriptionRegex.exec(documentationTagsSection);
-  return descriptionMatch?.[1] || '';
-};
