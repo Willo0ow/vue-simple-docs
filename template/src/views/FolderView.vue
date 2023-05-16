558b8c0c7968654c4966f9ced502bf86f6fd0495
Marta Kurian-Herma <marta.kurian-herma@monterail.com>
2023-05-16 12:44:15 +0200

    Remove unused code

--- template/src/views/FolderView.vue
+++ template/src/views/FolderView.vue
@@ -1,6 +1,6 @@
 <template>
   <v-list-item>
-    <v-list-item-title lines="two">{{ props.name }}</v-list-item-title>
+    <v-list-item-title>{{ props.name }}</v-list-item-title>
   </v-list-item>
   <v-list v-for="item of dirContent" :key="item.name" class="ml-5">
     <tree-item :key="item.name" :item="item" />
